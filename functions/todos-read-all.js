f43306205172fb641d4c5a153fa05e601ffc4dfb
Jon Hogue <jon@hogue.org>
2021-08-08 15:48:22 -0400

    remove console logs

--- functions/todos-read-all.js
+++ functions/todos-read-all.js
@@ -4,17 +4,13 @@ const q = faunadb.query
 
 
 exports.handler = (event, context) => {
-  console.log('Function `todo-read-all` invoked')
   /* configure faunaDB Client with our secret */
-  console.log('secret:' + process.env.FAUNADB_SERVER_SECRET)
   const client = new faunadb.Client({
     secret: process.env.FAUNADB_SERVER_SECRET
   }) 
   return client.query(q.Paginate(q.Match(q.Ref('indexes/all_todos'))))
     .then((response) => {
       const todoRefs = response.data
-      console.log('Todo refs', todoRefs)
-      console.log(`${todoRefs.length} todos found`)
       // create new query out of todo refs. http://bit.ly/2LG3MLg
       const getAllTodoDataQuery = todoRefs.map((ref) => {
         return q.Get(ref)
@@ -27,7 +23,6 @@ exports.handler = (event, context) => {
         }
       })
     }).catch((error) => {
-      console.log('error', error)
       return {
         statusCode: 400,
         body: JSON.stringify(error)
